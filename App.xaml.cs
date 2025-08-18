65c3bac621cb4e6c6f2078d0695a978fc353c52c
小宮山竣也 <ks@ooyuminamichoshinoMacBook-Air.local>
2025-08-18 11:22:11 +0900

    chore: initialize .NET MAUI (iOS) project

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ShogiKifuApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ShogiKifuApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
